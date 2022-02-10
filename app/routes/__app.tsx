08b387b8ecc81a6ffd23413fb39d4d632ab9a3c7
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2022-02-09 21:51:15 -0500

    initial commit

---
+++ app/routes/__app.tsx
@@ -0,0 +1,39 @@
+import { Outlet } from 'remix';
+import type { LoaderFunction, MetaFunction } from 'remix';
+
+// pass into React Context?
+import { fetchAPI } from '../../lib/api';
+import { getStrapiMedia } from '../../lib/media';
+
+import { Navbar, Footer } from '~/components';
+
+export const loader: LoaderFunction = async () => {
+  const globalRes = await fetchAPI('/global', {
+    // figure out this object structure vs the json object output
+    populate: {
+      favicon: '*',
+      defaultSeo: {
+        populate: '*',
+      },
+    },
+  });
+
+  return globalRes.data;
+};
+
+export const meta: MetaFunction = ({ data }) => {
+  return {
+    title: `${data.attributes.defaultSeo.metaTitle}`,
+    description: `${data.attributes.defaultSeo.metaDescription}`,
+  };
+};
+
+export default function Index() {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
---
+++ app/routes/__app/index.tsx
@@ -0,0 +1,37 @@
+import { useLoaderData } from 'remix';
+import type { LoaderFunction, MetaFunction } from 'remix';
+import { fetchAPI } from '../../../lib/api';
+import { getStrapiMedia } from '../../../lib/media';
+
+export const loader: LoaderFunction = async () => {
+  const globalRes = await fetchAPI('/global', {
+    populate: {
+      favicon: '*',
+      defaultSeo: {
+        populate: '*',
+      },
+    },
+  });
+
+  return globalRes.data;
+};
+
+export default function Index() {
+  const global = useLoaderData();
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello Remix</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Turpis egestas
+        pretium aenean pharetra. Orci eu lobortis elementum nibh tellus
+        molestie. Vulputate dignissim suspendisse in est. Vel pharetra vel
+        turpis nunc. Malesuada nunc vel risus commodo. Nisi vitae suscipit
+        tellus mauris. Posuere morbi leo urna molestie at elementum eu. Urna
+        duis convallis convallis tellus. Urna molestie at elementum eu. Nunc sed
+        blandit libero volutpat.
+      </p>
+    </div>
+  );
+}
