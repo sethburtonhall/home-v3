079385cf4e5e915ec1eb8f36bae1877db0e45708
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-02-24 13:44:32 -0700

    Hero edit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       </h1>
       {/* <p className="mb-6 text-center text-xl md:text-2xl">My name is Seth.</p> */}
       <p className="mb-6 text-base md:text-xl">
-        I am a multi-hyphenate creative (Film Producer-Web Developer-Technical Product Owner-Journeyman) currently working from the road while traveling with my family in a vintage 1994 Airstream Classic. 🛻 🏕 💻 🏞️</p>
+        I am a multi-hyphenate creative (Film Producer-Web Developer-Technical Product Owner-Journeyman) currently working from the road while traveling with my family in a vintage Airstream Classic. 💻 🏞️</p>
       <p className="mb-6 text-base md:text-xl">
         I value authentic communication and creative collaboration and believe a workplace should be supportive and empowering. I am currently looking for my next team and adventure. You can find me online at{' '}
         <a
