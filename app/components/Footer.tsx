3f98515b7a785df7eff7ab2b97428f65b5ce419e
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-02-24 14:02:15 -0700

    footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import { MadeWith } from '.';
 export default function Footer() {
   return (
     <footer
-      className="mt-auto flex w-full flex-col items-center px-4 py-6 text-base font-semibold md:flex-row"
+      className="mt-auto flex w-full flex-col items-center justify-between px-4 py-6 text-base font-semibold md:flex-row"
       role="contentinfo"
     >
       <SocialIcons />
