af9f26913eaa65e962dc80da5103db9c302a46d8
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2023-10-14 21:47:20 -0700

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,12 +2,12 @@ export default function Hero() {
   return (
     <div className="mx-auto flex w-full flex-col items-center xl:w-2/3">
       {/* Intro */}
-      <h1 className="mb-6 w-full text-center text-4xl md:text-5xl">
+{/*       <h1 className="mb-6 w-full text-center text-4xl md:text-5xl">
         Lead Frontend Developer
-      </h1>
+      </h1> */}
       {/* <p className="mb-6 text-center text-xl md:text-2xl">My name is Seth.</p> */}
-      <p className="text-base md:text-xl">
-        Frontend Developer working in composable. Creative Producer living in sabbatical. Digital Nomad. Journeyman. Over 25 years of experience in creative collaboration. You can find me online at{' '}
+      <p className="text-lg md:text-xl">
+        Lead Frontend Developer working in composable. Creative Producer living in sabbatical. Digital Nomad. Journeyman. Over 25 years of experience in creative collaboration. You can find me online at{' '}
         <a
           href="https://www.linkedin.com/in/sethburtonhall/"
           target="_blank"
